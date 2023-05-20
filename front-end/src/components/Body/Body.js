c899f317b165b9db6a25a5c880bb31249eae0150
abdallahibraheemsalameh <abdallahsalameh43@yahoo.com>
2023-05-20 18:06:22 +0300

    Add BodySectionOne component

--- front-end/src/components/Body/Body.js
+++ front-end/src/components/Body/Body.js
@@ -1,10 +1,17 @@
 import React from "react";
 import bodyStyle from "./BodyStyle.module.css";
 import NavigationSection from "./NavigationSection";
+import BodySectionOne from "./BodySectionOne";
+
 export default function Body() {
   return (
     <div className={bodyStyle.bodyContainer}>
       <NavigationSection />
+      <div className={bodyStyle.bodySectionsContainer}>
+        <h4>Welcome! John Doe</h4>
+        {/* here new component ⬇ */}
+        <BodySectionOne />
+      </div>
     </div>
   );
 }
